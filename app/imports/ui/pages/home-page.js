85bc9c473fcaebaeeb271701f5bd8ac0acfbf16f
Alan Tam <teetree89@gmail.com>
2017-04-29 14:49:16 -1000

    calculation for radince per hour on current day.

--- app/imports/ui/pages/home-page.js
+++ app/imports/ui/pages/home-page.js
@@ -2,6 +2,15 @@ import { Template } from 'meteor/templating';
 import { Weather } from '../../api/weather/weather.js';
 import { Meteor } from 'meteor/meteor';
 
+function dayOfYear(d) {   // d is a Date object
+    const yn = d.getFullYear();
+    const mn = d.getMonth();
+    const dn = d.getDate();
+    const d1 = new Date(yn,0,1,12,0,0); // noon on Jan. 1
+    const d2 = new Date(yn,mn,dn,12,0,0); // noon on input date
+    const ddiff = Math.round((d2-d1)/864e5);
+    return ddiff+1; }
+
 Template.Home_Page.helpers({
   errorClass() {
     return Template.instance().messageFlags.get(displayErrorMessages) ? 'error' : '';
@@ -17,6 +26,15 @@ Template.Home_Page.helpers({
   weather() {
     const w = Weather.find().fetch();
     return w[0];
+  },
+  efficiency() {
+    const w_temp = Weather.find().fetch();
+    const w = w_temp[0];
+    const cloud_percent = parseFloat(w.clouds)/100.00;
+    const day = dayofYear(new Date());
+    const rad_ind = day*24;
+    const actual_radiation = cloud_percent * w.radiation[rad_ind];
+    //insert actual radiation into weather collection.return actual_radiation;
   },
 });
 
