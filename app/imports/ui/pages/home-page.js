82f5bb2861d410e954104b8552bcf7068c6e3112
Amy Takayesu <amytakayesu@gmail.com>
2017-04-29 18:29:51 -1000

    Added calculations

--- app/imports/ui/pages/home-page.js
+++ app/imports/ui/pages/home-page.js
@@ -18,6 +18,30 @@ Template.Home_Page.helpers({
     const w = Weather.find().fetch();
     return w[0];
   },
+  efficiency() {
+    const w_temp = Weather.find().fetch();
+    const w = w_temp[0];
+    const cloud_percent = parseFloat(w.clouds)/100.00;
+    // const day = dayofYear(new Date());
+    // const rad_ind = parseInt(day)*24;
+    const p = (1 - (0.75 * (Math.pow(cloud_percent, 3))));
+    const actual_radiation = p * parseFloat(w.radiation);
+    // get from ui total_consumption per hour
+    let total_consumption = 500;
+    // get panelEff from ui
+    let panelEff = .20;
+    // assign total_consumption or return it
+
+    // get panel size from ui
+    const panelSize = 8;
+    const total_energy = actual_radiation*panelSize*panelEff - total_consumption;
+    console.log(cloud_percent);
+    console.log(p);
+    console.log(actual_radiation);
+    console.log(actual_radiation*panelSize*panelEff);
+    console.log(total_energy);
+    return total_energy;
+  },
 });
 
 Template.Home_Page.onCreated(function onCreated() {
