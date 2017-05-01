921c4ecccdd99db944d881ff44cb85fafe86d008
Amy Takayesu <amytakayesu@gmail.com>
2017-04-30 14:22:20 -1000

    Grab data from sliders with button
    Will figure out without button later

--- app/imports/ui/pages/electric-consumption.js
+++ app/imports/ui/pages/electric-consumption.js
@@ -2,12 +2,22 @@
  * Created by mike3 on 4/29/2017.
  */
 import { Template } from 'meteor/templating';
+import { Weather } from '../../api/weather/weather.js';
 
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     //$('.modal-trigger').leanModal();
     //$('.modal1').modal();
 
+});
+
+function updateValue(newVal, deviceName) {
+  console.log('Slider Value: ' + newVal);
+  document.getElementById(deviceName).innerHTML = newVal;
+}
+
+Template.Electric_Consumption_Page.onCreated(function onCreated() {
+  this.subscribe(Weather.getPublicationName());
 });
 
 Template.Electric_Consumption_Page.onRendered(function onRendered() {
@@ -24,11 +34,32 @@ Template.Electric_Consumption_Page.onRendered(function onRendered() {
 //let device3 = {"Large Appliance", 30, 1000};
 
 Template.Electric_Consumption_Page.helpers({
-
-    addDevice: function(name, time, power){
-
+    devices(){
+      const w = Weather.find().fetch();
+      console.log(w[0].devices);
+      return w[0].devices;
     },
+  updateValue(newVal, deviceName) {
+    console.log('Slider Value: ' + newVal);
+    //document.getElementById(deviceName).innerHTML = newVal;
+  },
+});
 
+Template.Electric_Consumption_Page.events({
+  'click .device-sliders'(event, instance) {
+    event.preventDefault();
+    // Get name (text field)
+    const weathers = Weather.find().fetch();
+    const weather = weathers[0];
+    let newDevices = weather.devices;
 
-
+    console.log(weather.devices);
+    for (i=0; i < weather.devices.length; i++) {
+      console.log('value: ' + document.getElementById(`${weather.devices[i].name}-slider`).value);
+      newDevices[i].time = document.getElementById(`${weather.devices[i].name}-slider`).value;
+    }
+    Weather.update(weather._id, {
+      $set: { devices: newDevices },
+    });
+  },
 });
