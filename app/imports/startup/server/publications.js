e2596aee8196fd9545a675daafe62b38cdf9b8fe
Amy Takayesu <amytakayesu@gmail.com>
2017-05-03 19:37:26 -1000

    colored bars work nowyay

--- app/imports/startup/server/publications.js
+++ app/imports/startup/server/publications.js
@@ -31,7 +31,6 @@ Meteor.methods({
         for (var k = 0; k < 24; k++) {
           radiationForecast[j] += radiationArray[((theDay + j + 1) * 24) + k];
         }
-        console.log('Radiation Forecast j ' + (radiationForecast[j]/24));
         radiationForecast[j] = Math.round((radiationForecast[j]/24));
       }
 
@@ -54,55 +53,18 @@ Meteor.methods({
       const clouds = response.data.clouds.all;
       const name = response.data.name;
       const radiation = avgHourly;
-      const devices = [{ name:'Lab', power:5000, time:0 },
-        { name:'Small Device', power:330, time:0 },
-        { name:'Large Device', power:35000, time:0 }];
-      console.log(devices);
-      const weather = { latitude, longitude, description, temperature, windSpeed, clouds, name, radiation, devices, radiationForecast };
+      //console.log(devices);
+      const weather = { latitude, longitude, description, temperature, windSpeed, clouds, name, radiation, radiationForecast };
       if (Weather.find().count() === 0) {
         Weather.define(weather);
       } else {
         const weathers = Weather.find().fetch();
         const weather = weathers[0];
         Weather.update(weather._id, {
-          $set: { latitude, longitude, description, temperature, windSpeed, clouds, name, radiation, devices, radiationForecast },
+          $set: { latitude, longitude, description, temperature, windSpeed, clouds, name, radiation, radiationForecast },
         });
       }
       return weather;
-    } catch (e) {
-      // Got a network error, timeout, or HTTP error in the 400 or 500 range.
-      return false;
-    }
-  },
-  checkRadiation(latitude, longitude) {
-    check(latitude, String);
-    check(longitude, String);
-    //this.unblock();
-    console.log('hiiiii');
-    try {
-      const weathers = Weather.find().fetch();
-      const weather = weathers[0];
-      const response = HTTP.get(`https://developer.nrel.gov/api/pvwatts/v5.json?api_key=AlyMNtdT59V5xJq0rG52mYVUjPMf2uuLIQi7ScRI&lat=${latitude}&lon=${longitude}&system_capacity=4&azimuth=180&tilt=40&array_type=1&module_type=1&losses=10&timeframe=hourly`);
-      const radiation = response.data.outputs.dn;
-      const today = new Date();
-      const first = new Date(today.getFullYear(), 0, 1);
-      const theDay = Math.round(((today - first) / 1000 / 60 / 60 / 24) + .5, 0);
-      const d = new Date();
-      const n = d.getHours();
-      const index = (theDay * 24) + n;
-      console.log(radiation[index]);
-      const newWeather = {
-        description: weather.description,
-        temperature: weather.temperature,
-        windSpeed: weather.windSpeed,
-        clouds: weather.clouds,
-        name: weather.name,
-        radiation: radiation[index],
-      };
-      Weather.remove({});
-      Weather.insert(newWeather);
-      console.log('hi');
-      return true;
     } catch (e) {
       // Got a network error, timeout, or HTTP error in the 400 or 500 range.
       return false;
